ca1d1febe4988ddbd1034f1861816440b19aa8e0
ka1i <Mardan137@outlook.com>
2021-10-13 00:24:07 +0800

    texture & color overlay

--- internal/app/win/ShaderSource.go
+++ internal/app/win/ShaderSource.go
@@ -39,14 +39,14 @@ const (
 	fragmentShaderSource = `
 		#version 330 core
 		out vec4 fragmentColor;
-
+		in vec3 vertexColor; // 从顶点着色器传来的输入变量（名称相同、类型相同）
 		in vec2 TexCoord;
 
 		uniform sampler2D ourTexture;
 
 		void main()
 		{
-			fragmentColor = texture(ourTexture, TexCoord);
+			fragmentColor = texture(ourTexture, TexCoord) * vec4(vertexColor, 1.0);
 		} 
 	` + "\x00"
 )
